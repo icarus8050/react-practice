0c932d62ac4817d8890c160a121fad6694535688
icarus8050 <icarus8050@naver.com>
2020-02-28 23:58:29 +0900

    시간 여행 구현하기

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ class App extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (CalculateWinner(squares) || squares[i]) {
@@ -25,14 +26,33 @@ class App extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = CalculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        });
 
         let status;
         if (winner) {
@@ -52,7 +72,7 @@ class App extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
