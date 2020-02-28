f3a22313a0d374fb5549865d367e224b55129468
icarus8050 <icarus8050@naver.com>
2020-02-28 23:39:18 +0900

    Board 의 Status 를 App 으로 다시 끌어올리기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import Board from './components/Board'
+import CalculateWinner from "./components/Calculator";
 
 class App extends React.Component {
-  render() {
-    return (
-        <div className="game">
-            <div className="game-board">
-                <Board/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        }
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (CalculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = CalculateWinner(current.squares);
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                    />
+
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{}</ol>
+                </div>
             </div>
-            <div className="game-info">
-                <div>{}</div>
-                <ol>{}</ol>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
