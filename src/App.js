366f1a15be3b95f6473aecfbfc69f1e6257294fa
icarus8050 <icarus8050@naver.com>
2021-09-19 01:59:16 +0900

    userState

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Component} from "react";
 import MyComponent from "./component/MyComponent";
 import MySecondComponent from "./component/MySecondComponent"
 import Counter from "./component/Counter";
+import Say from "./component/Say";
 
 class App extends Component {
     render() {
@@ -14,6 +15,8 @@ class App extends Component {
                 <MySecondComponent favoriteNumber={3}>리액트</MySecondComponent>
                 <hr/>
                 <Counter name={counter} number={10}/>
+                <hr/>
+                <Say/>
             </div>
         );
     }
