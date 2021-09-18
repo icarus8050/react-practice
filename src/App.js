185d0b3514b3943ebd8eff285c0f1e8b8f703351
icarus8050 <icarus8050@naver.com>
2021-09-19 01:06:49 +0900

    클래스형 컴포넌트 PropsType

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css'
 import {Component} from "react";
 import MyComponent from "./component/MyComponent";
+import MySecondComponent from "./component/MySecondComponent"
 
 class App extends Component {
     render() {
         return (
-            <MyComponent name="React" favoriteNumber={7}>리액트</MyComponent>
+            <div>
+                <MyComponent name="React" favoriteNumber={7}>리액트</MyComponent>
+                <hr/>
+                <MySecondComponent favoriteNumber={3}>리액트</MySecondComponent>
+            </div>
         );
     }
 }
