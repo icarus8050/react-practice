a51eae35dbfb06b68ffdbc0c42152414f8877cea
icarus8050 <icarus8050@naver.com>
2021-09-24 03:25:07 +0900

    반응형 styled

--- src/component/EmotionComponent.js
+++ src/component/EmotionComponent.js
@@ -1,10 +1,29 @@
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768
+}
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+      @media (max-width: ${sizes[label] / 16}em) {
+        ${css(...args)};
+      }
+    `;
+
+    return acc;
+}, {});
+
 const Box = styled.div`
   background: ${props => props.color || 'blue'};
   padding: 1rem;
   display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  ${media.desktop`width: 768px;`};
+  ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
@@ -35,6 +54,7 @@ const Button = styled.button`
               color: black;
             }
           `};
+
   & + button {
     margin-left: 1rem;
   }
